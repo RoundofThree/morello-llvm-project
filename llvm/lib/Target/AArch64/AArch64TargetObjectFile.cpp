0977c5df15f7d36152e78ee3166f44375b2abefa
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2024-03-03 20:55:09 -0800

    Add missing include after upstream updates

--- llvm/lib/Target/AArch64/AArch64TargetObjectFile.cpp
+++ llvm/lib/Target/AArch64/AArch64TargetObjectFile.cpp
@@ -10,6 +10,7 @@
 #include "AArch64TargetMachine.h"
 #include "MCTargetDesc/AArch64TargetStreamer.h"
 #include "llvm/BinaryFormat/Dwarf.h"
+#include "llvm/BinaryFormat/ELF.h"
 #include "llvm/IR/Mangler.h"
 #include "llvm/MC/MCContext.h"
 #include "llvm/MC/MCExpr.h"
