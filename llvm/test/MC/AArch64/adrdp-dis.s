67361ebe67dcf137be7acb2e2c9c5e15fdbc4a14
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2024-03-12 23:04:52 -0700

    [Morello] Update test for new llvm-objdump output format

--- llvm/test/MC/AArch64/adrdp-dis.s
+++ llvm/test/MC/AArch64/adrdp-dis.s
@@ -1,8 +1,8 @@
 // RUN: llvm-mc -arch=aarch64 -filetype=obj -mattr=+morello,+c64 -target-abi purecap %s -o - | llvm-objdump -d - | FileCheck %s
 
 // CHECK: 0000000000001000 <foo>:
-// CHECK-NEXT:    1000: 06 00 00 d0  	adrdp	c6, 0x2000
-// CHECK-NEXT:    1004: c6 30 40 c2  	ldr	c6, [c6, #192]
+// CHECK-NEXT:    1000: d0000006  	adrdp	c6, 0x2000
+// CHECK-NEXT:    1004: c24030c6  	ldr	c6, [c6, #192]
 
 .globl bar
  nop
