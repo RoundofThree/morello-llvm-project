rems-project/llvm-project

commit b3e7e835d7e8fd774c4b1a1274c82b527bffe1ab

Use R_MORELLO_RELATIVE for capabilility initialization in static executables

lld/ELF/SyntheticSections.cpp
@@ -1347,6 +1347,8 @@ void DynamicSection<ELFT>::addSym(int32_t tag, Symbol *sym) {
 // - in.relaIplt: this is included if in.relaIplt is named .rela.dyn
 // - in.relaPlt: this is included if a linker script places .rela.plt inside
 //   .rela.dyn
+// - in.relaDyn: this is included if R_MORELLO_RELATIVE relocations are
+//   created.
 //
 // DT_RELASZ is the total size of the included sections.
 static std::function<uint64_t()> addRelaSz(RelocationBaseSection *relaDyn) {
@@ -1356,6 +1358,8 @@ static std::function<uint64_t()> addRelaSz(RelocationBaseSection *relaDyn) {
       size += in.relaIplt->getSize();
     if (in.relaPlt->getParent() == relaDyn->getParent())
       size += in.relaPlt->getSize();
+    if (in.relaDyn->getParent() == relaDyn->getParent())
+      size += in.relaDyn->getSize();
     return size;
   };
 }
@@ -1369,6 +1373,10 @@ static uint64_t addPltRelSz() {
   if (in.relaIplt->getParent() == in.relaPlt->getParent() &&
       in.relaIplt->name == in.relaPlt->name)
     size += in.relaIplt->getSize();
+
+  if (in.relaDyn->getParent() == in.relaPlt->getParent() &&
+      (in.relaDyn->name == in.relaPlt->name))
+    size += in.relaDyn->getSize();
   return size;
 }
 
@@ -1452,7 +1460,9 @@ template <class ELFT> void DynamicSection<ELFT>::finalizeContents() {
 
   if (part.relaDyn->isNeeded() ||
       (in.relaIplt->isNeeded() &&
-       part.relaDyn->getParent() == in.relaIplt->getParent())) {
+       part.relaDyn->getParent() == in.relaIplt->getParent()) ||
+      (in.relaDyn->isNeeded() &&
+       part.relaDyn->getParent() == in.relaDyn->getParent())) {
     addInSec(part.relaDyn->dynamicTag, part.relaDyn);
     entries.push_back({part.relaDyn->sizeDynamicTag, addRelaSz(part.relaDyn)});
 
@@ -1483,7 +1493,8 @@ template <class ELFT> void DynamicSection<ELFT>::finalizeContents() {
   // as relaIplt has. And we still want to emit proper dynamic tags for that
   // case, so here we always use relaPlt as marker for the beginning of
   // .rel[a].plt section.
-  if (isMain && (in.relaPlt->isNeeded() || in.relaIplt->isNeeded())) {
+  if (isMain && (in.relaPlt->isNeeded() || in.relaIplt->isNeeded() ||
+                 in.relaDyn->isNeeded())) {
     addInSec(DT_JMPREL, in.relaPlt);
     entries.push_back({DT_PLTRELSZ, addPltRelSz});
     switch (config->emachine) {
@@ -1812,8 +1823,21 @@ void RelocationBaseSection::finalizeContents() {
     if (config->isCheriAbi && in.cheriCapTable && in.cheriCapTable->isNeeded()) {
       assert(in.cheriCapTable->getParent()->sectionIndex != UINT32_MAX);
       getParent()->info = in.cheriCapTable->getParent()->sectionIndex;
+      if (in.relaIplt == this)
+        getParent()->info = in.cheriCapTable->getParent()->sectionIndex;
+      if (in.relaDyn == this)
+        getParent()->info = in.cheriCapTable->getParent()->sectionIndex;
     } else {
-      getParent()->info = in.gotPlt->getParent()->sectionIndex;
+      if (in.relaPlt == this)
+        getParent()->info = in.gotPlt->getParent()->sectionIndex;
+    }
+    if (in.relaDyn == this) {
+      if (in.igotPlt && in.igotPlt->isNeeded())
+        getParent()->info = in.igotPlt->getParent()->sectionIndex;
+      else if (!config->hasDynSymTab)
+        // In Morello static linking, the relaDyn can be used without the GOT or
+        // PLTGOT
+        getParent()->info = 0;
     }
   }
   if (in.relaIplt == this) {
@@ -1822,9 +1846,12 @@ void RelocationBaseSection::finalizeContents() {
     if (config->isCheriAbi && in.cheriCapTable && in.cheriCapTable->isNeeded()) {
       assert(in.cheriCapTable->getParent()->sectionIndex != UINT32_MAX);
       getParent()->info = in.cheriCapTable->getParent()->sectionIndex;
-    } else {
+    } else if (in.igotPlt && in.igotPlt->isNeeded())
       getParent()->info = in.igotPlt->getParent()->sectionIndex;
-    }
+    else if (!config->hasDynSymTab)
+      // In Morello static linking, the relaDyn can be used without the GOT or
+      // PLTGOT
+      getParent()->info = 0;
   }
 }
 

lld/ELF/Arch/Cheri.cpp
@@ -402,11 +402,22 @@ static uint64_t getTargetSize(const CheriCapRelocLocation &location,
     if (isAbsoluteSym)
       return targetSize;
 
-    // Morello may store symbol size in lower 8-bytes of the 16-byte frag
-    // reserved by .capinit
     if (config->emachine == EM_AARCH64 && !targetSym->isInGot()) {
+      // For caprelocs, the Morello linker obtains the symbol size from the
+      // lower 8-bytes of a 16-byte frag reserved by .capinit (buf+8).
+
+      // For dynamic relocations, the linker breaks the 16-byte frag into two
+      // 8-byte locations (see addMorelloCapabilityFragment()) and obtains the
+      // size from the second of these locations when processing the
+      // R_MORELLO_CAPFRAG_SIZE_AND_PERM internal  static relocation (see
+      // getMorelloSizeAndPermissions()). So (buf) can be used because it
+      // already has the 8 byte offset built into it.
+
       const uint8_t *buf = location.section->data().begin() + location.offset;
-      targetSize = read64le(buf + 8);
+      targetSize = ((config->localCapRelocsMode == CapRelocsMode::Legacy) &&
+                    !config->shared)
+                       ? read64le(buf + 8)
+                       : read64le(buf);
       if (targetSize)
         return targetSize;
     }
@@ -925,32 +936,40 @@ void addMorelloCapabilityFragment(InputSectionBase *sec, Symbol *sym,
 static void addCapDynamicRelocation(RelType dynType, Symbol *sym,
                                     InputSectionBase *sec, uint64_t offset,
                                     int64_t addend) {
-  // The symbol VA is used in case of a relative relocation against an
-  // executable symbol. However the VA undergoes another transformation
-  // so that the fragment pointed by the relocation is representable by the
-  // CHERI Concentrate compression scheme. See calcAddend().
   bool isExecRel =
       (sym->isFunc() || sym->isGnuIFunc()) &&
       (dynType == R_MORELLO_RELATIVE || dynType == R_MORELLO_IRELATIVE);
-  mainPart->relaDyn->addReloc({dynType, sec, offset,
-                               isExecRel ? DynamicReloc::AArch64ExecRel
-			                 : DynamicReloc::AgainstSymbol,
-                               *sym, addend, R_ABS});
+  if (dynType == R_MORELLO_RELATIVE && !sym->includeInDynsym() &&
+      config->localCapRelocsMode == CapRelocsMode::ElfReloc) {
+    in.relaDyn->addReloc(
+        {dynType, sec, offset,
+         isExecRel ? DynamicReloc::AArch64ExecRel : DynamicReloc::AgainstSymbol,
+         *sym, addend, R_ABS});
+  } else {
+    mainPart->relaDyn->addReloc(
+        {dynType, sec, offset,
+         isExecRel ? DynamicReloc::AArch64ExecRel : DynamicReloc::AgainstSymbol,
+         *sym, addend, R_ABS});
+  }
   addMorelloCapabilityFragment(sec, sym, offset, isExecRel);
 }
 
 // Relocation arising from addGotEntry() or addPltEntry().
 // This can happen for both static and dynamic linking as capabilities can only
 // be initialized at run-time.
-void addMorelloC64GotRelocation(RelType dynType, Symbol *sym, InputSectionBase *sec, uint64_t offset) {
+void addMorelloC64GotRelocation(RelType dynType, Symbol *sym,
+                                InputSectionBase *sec, uint64_t offset,
+                                int64_t addend) {
   // If there is a Dynamic Symbol Table, there cannot be a caprelocs section.
   // R_MORELLO_IRELATIVE can be present even without a Dynamic Symbol Table
   // being present.
-  if (config->hasDynSymTab || dynType == R_MORELLO_IRELATIVE)
-    addCapDynamicRelocation(dynType, sym, sec, offset, 0);
-  else
-    in.capRelocs->addCapReloc({sec, offset}, {sym, 0u},
-                              sym->isPreemptible, 0);
+  if (config->hasDynSymTab || dynType == R_MORELLO_IRELATIVE ||
+      config->localCapRelocsMode == CapRelocsMode::ElfReloc) {
+    addCapDynamicRelocation(dynType, sym, sec, offset, addend);
+  } else {
+    in.capRelocs->addCapReloc({sec, offset}, {sym, 0u}, sym->isPreemptible,
+                              addend);
+  }
 }
 
 // For the .capinit R_MORELLO_CAPINIT relocation. Called from the
@@ -958,17 +977,12 @@ void addMorelloC64GotRelocation(RelType dynType, Symbol *sym, InputSectionBase *
 static void addMorelloCapabilityRelocation(Symbol *sym, RelType type,
                                          InputSectionBase *sec, uint64_t offset,
                                          int64_t addend) {
-  if (config->hasDynSymTab) {
-    // When dynamic linking we propagate the R_MORELLO_CAPINIT if the symbol is
-    // preemptible, otherwise we use R_MORELLO_RELATIVE.
-    RelType dynType = (sym->includeInDynsym() && sym->isPreemptible)
-                          ? R_MORELLO_CAPINIT
-                          : R_MORELLO_RELATIVE;
-    addCapDynamicRelocation(dynType, sym, sec, offset, addend);
-  } else {
-    in.capRelocs->addCapReloc({sec, offset}, {sym, 0u},
-                              sym->isPreemptible, addend);
-  }
+  // When dynamic linking we propagate the R_MORELLO_CAPINIT if the symbol is
+  // preemptible, otherwise we use R_MORELLO_RELATIVE.
+  RelType dynType = (sym->includeInDynsym() && sym->isPreemptible)
+                        ? R_MORELLO_CAPINIT
+                        : R_MORELLO_RELATIVE;
+  addMorelloC64GotRelocation(dynType, sym, sec, offset, addend);
 }
 
 MorelloTLSLEDataSection::MorelloTLSLEDataSection()

lld/ELF/Arch/Cheri.h
@@ -403,7 +403,9 @@ void addCapabilityRelocation(Symbol *sym, RelType type, InputSectionBase *sec,
 
 // Emit either a dynamic relocation or __cap_reloc entry to initialize a
 // GOT slot.
-void addMorelloC64GotRelocation(RelType dynType, Symbol *sym, InputSectionBase *sec, uint64_t offset);
+void addMorelloC64GotRelocation(RelType dynType, Symbol *sym,
+                                InputSectionBase *sec, uint64_t offset,
+                                int64_t addend);
 void addMorelloCapabilityFragment(InputSectionBase *sec, Symbol *sym,
                                   uint64_t offset, bool isExecRel);
 

lld/ELF/Driver.cpp
@@ -366,8 +366,10 @@ static void checkOptions() {
     if (config->exportDynamic)
       error("-r and --export-dynamic may not be used together");
   }
-  if (config->localCapRelocsMode == CapRelocsMode::ElfReloc)
-    error("local-cap-relocs=elf is not implemented yet");
+  if (config->emachine != EM_AARCH64)
+    if (config->localCapRelocsMode == CapRelocsMode::ElfReloc)
+      error("local-cap-relocs=elf is not implemented yet");
+
   if (config->localCapRelocsMode == CapRelocsMode::CBuildCap)
     error("local-cap-relocs=cbuildcap is not implemented yet");
   assert(config->preemptibleCapRelocsMode != CapRelocsMode::CBuildCap);
@@ -387,6 +389,10 @@ static void checkOptions() {
 
   if (config->zRetpolineplt && config->zForceIbt)
     error("-z force-ibt may not be used with -z retpolineplt");
+
+  if (config->localCapRelocsMode == CapRelocsMode::CBuildCap) {
+    error("--morello-static-caps=cbuildcap is not supported");
+  }
 
   if (config->emachine != EM_AARCH64) {
     if (config->zPacPlt)
@@ -1079,6 +1085,7 @@ static void readConfigs(opt::InputArgList &args) {
                    /* Default */ true);
   config->allowUndefinedCapRelocs = args.hasArg(OPT_allow_undefined_cap_relocs);
   config->forceMorelloC64Plt = args.hasArg(OPT_morello_c64_plt);
+  config->localCapRelocsMode = getLocalCapRelocsMode(args);
   config->auxiliaryList = args::getStrings(args, OPT_auxiliary);
   if (opt::Arg *arg = args.getLastArg(OPT_Bno_symbolic, OPT_Bsymbolic_functions,
                                       OPT_Bsymbolic)) {

lld/ELF/Relocations.cpp
@@ -1193,7 +1193,7 @@ static void addGotEntry(Symbol &sym) {
       // a capability. When dynamic linking the dynamic loader creates the
       // capability, when static linking the function adds an entry to the
       // __cap_relocs table.
-      addMorelloC64GotRelocation(target->relativeRel, &sym, in.got, off);
+      addMorelloC64GotRelocation(target->relativeRel, &sym, in.got, off, 0);
     else
       in.got->relocations.push_back({expr, target->symbolicRel, off, 0, &sym});
     return;
@@ -1206,7 +1206,7 @@ static void addGotEntry(Symbol &sym) {
       // As well as writing the Relative relocation there are additional
       // static relocations needed to initialize the GOT entry. Delegate this
       // to addMorelloC64GotRelocation.
-      addMorelloC64GotRelocation(target->relativeRel, &sym, in.got, off);
+      addMorelloC64GotRelocation(target->relativeRel, &sym, in.got, off, 0);
     else
       addRelativeReloc(in.got, off, sym, 0, R_ABS, target->symbolicRel);
     return;
@@ -1215,7 +1215,7 @@ static void addGotEntry(Symbol &sym) {
     // As with the Relative relocation there are additional static relocations
     // needed to initialize the GOT entry. We do not do this for TLS symbols as
     // the GOT generating TLS relocation is not a capability (offset from TP).
-    addMorelloC64GotRelocation(target->gotRel, &sym, in.got, off);
+    addMorelloC64GotRelocation(target->gotRel, &sym, in.got, off, 0);
   else
     mainPart->relaDyn->addAddendOnlyRelocIfNonPreemptible(
         sym.isTls() ? target->tlsGotRel : target->gotRel, in.got, off, sym,

lld/ELF/Symbols.cpp
@@ -142,6 +142,9 @@ Defined *ElfSym::newLibBss1;
 Defined *ElfSym::newLibBss2;
 Defined *ElfSym::newLibEnd;
 
+Defined *ElfSym::relaDynStart;
+Defined *ElfSym::relaDynEnd;
+
 static uint64_t getSymVA(const Symbol &sym, int64_t &addend) {
   switch (sym.kind()) {
   case Symbol::DefinedKind: {

lld/ELF/Symbols.h
@@ -491,6 +491,10 @@ struct ElfSym {
   static Defined *newLibBss1;
   static Defined *newLibBss2;
   static Defined *newLibEnd;
+
+  // __rela_dyn_{start,end} symbols.
+  static Defined *relaDynStart;
+  static Defined *relaDynEnd;
 };
 
 // A buffer class that is large enough to hold any Symbol-derived

lld/ELF/SyntheticSections.h
@@ -1278,6 +1278,7 @@ struct InStruct {
   IBTPltSection *ibtPlt;
   RelocationBaseSection *relaPlt;
   RelocationBaseSection *relaIplt;
+  RelocationBaseSection *relaDyn;
   StringTableSection *shStrTab;
   StringTableSection *strTab;
   SymbolTableBaseSection *symTab;

RoundofThree/morello-llvm-project

commit 7e10a9b5b3919d10a5310cb9fe981e79f07b36d1

lld: Don't emit empty DT_{JMPREL,PLTRELSZ,PLTGOT} with ELF caprelocs
These entries shouldn't be emitted just because rela.dyn is non-empty.
There needs to be an actual PLT.

Also, the rela.dyn section for caprelocs is not used by PLT entries
and should not be part of DT_PLTRELSZ.

Fixes: b3e7e835d7e8 Use R_MORELLO_RELATIVE for capabilility initialization in static executables

lld/ELF/SyntheticSections.cpp
@@ -1394,10 +1394,6 @@ static uint64_t addPltRelSz() {
   if (in.relaIplt->getParent() == in.relaPlt->getParent() &&
       in.relaIplt->name == in.relaPlt->name)
     size += in.relaIplt->getSize();
-
-  if (in.relaDyn->getParent() == in.relaPlt->getParent() &&
-      (in.relaDyn->name == in.relaPlt->name))
-    size += in.relaDyn->getSize();
   return size;
 }
 
@@ -1523,8 +1519,7 @@ DynamicSection<ELFT>::computeContents() {
   // as relaIplt has. And we still want to emit proper dynamic tags for that
   // case, so here we always use relaPlt as marker for the beginning of
   // .rel[a].plt section.
-  if (isMain && (in.relaPlt->isNeeded() || in.relaIplt->isNeeded() ||
-                 in.relaDyn->isNeeded())) {
+  if (isMain && (in.relaPlt->isNeeded() || in.relaIplt->isNeeded())) {
     addInSec(DT_JMPREL, *in.relaPlt);
     entries.emplace_back(DT_PLTRELSZ, addPltRelSz());
     switch (config->emachine) {